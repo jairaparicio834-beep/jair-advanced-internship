faa1007ad6f2c924b0e087d13559444278d772ae
Jair Aparicio <jairaparicio834@gmail.com>
2026-04-26 19:52:11 -0700

    Changes

--- app/(app)/movie/[id]/page.tsx
+++ app/(app)/movie/[id]/page.tsx
@@ -1,27 +1,24 @@
-
 import React from 'react';
 import MovieDetails from './MovieDetails';
 import axios from 'axios';
-
 
 const fetchMovie = async (id: string) => {
     const { data } = await axios.get(`https://advanced-internship-api-production.up.railway.app/movies/${id}`)
     return data.data
 }
+
 interface PageProps {
     params: Promise<{
         id: string
     }>
 }
+
 const Page = async ({ params }: PageProps) => {
     const { id } = await params
-    console.log('MOVIE ID:', id)
     const movie = await fetchMovie(id)
-    console.log('MOVIE DATA:', movie)
 
     return (
         <MovieDetails movie={movie} />
-
     );
 }
 
